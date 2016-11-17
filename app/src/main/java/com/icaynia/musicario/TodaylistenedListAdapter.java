419c6c6eba2b1977043f25ace6a73dc1f7e56354
icaynia <icaynia@icaynia.com>
2016-11-18 00:59:45 +0900

    todaylistenedlistadapter initialize

---
+++ app/src/main/java/com/icaynia/musicario/TodaylistenedListAdapter.java
@@ -0,0 +1,57 @@
+package com.icaynia.musicario;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by icaynia on 2016. 11. 18..
+ */
+public class TodaylistenedListAdapter extends BaseAdapter {
+    List<MusicDto> list;
+    Activity activity;
+    LayoutInflater inflater;
+
+    public TodaylistenedListAdapter (Activity activity, List<MusicDto> list) {
+        this.list = list;
+        this.activity = activity;
+        inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.view_musiclist_row, parent, false);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            convertView.setLayoutParams(layoutParams);
+        }
+
+
+        return convertView;
+
+
+    }
+}
--- app/src/main/java/com/icaynia/musicario/ViewTodaylistened.java
+++ app/src/main/java/com/icaynia/musicario/ViewTodaylistened.java
@@ -2,13 +2,15 @@ package com.icaynia.musicario;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
 
 /**
  * Created by icaynia on 2016. 11. 17..
  */
 public class ViewTodaylistened extends LinearLayout {
-    
+    public Global global;
 
 
     public ViewTodaylistened(Context _context) {
@@ -22,6 +24,16 @@ public class ViewTodaylistened extends LinearLayout {
     }
 
     public void initialize() {
+        viewInitialize();
+    }
+
+    public void viewInitialize() {
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater li = (LayoutInflater) getContext().getSystemService(infService);
+        View v = li.inflate(R.layout.view_main_todaylistened, this, false);
+        addView(v);
+
+        global = (Global) getContext().getApplicationContext();
 
     }
 }
