d69b54a019af8d91c36814eb8ae42ed6e6ad52b4
icaynia <icaynia@icaynia.com>
2016-11-17 20:53:33 +0900

    add todaylistened class

---
+++ app/src/main/java/com/icaynia/musicario/ViewTodaylistened.java
@@ -0,0 +1,27 @@
+package com.icaynia.musicario;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by icaynia on 2016. 11. 17..
+ */
+public class ViewTodaylistened extends LinearLayout {
+    
+
+
+    public ViewTodaylistened(Context _context) {
+        super(_context);
+        initialize();
+    }
+
+    public ViewTodaylistened(Context _context, AttributeSet attrs) {
+        super(_context, attrs);
+        initialize();
+    }
+
+    public void initialize() {
+
+    }
+}
